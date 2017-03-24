52d4788d8f1d7ace38c88a3c08a51e7dea2393d1
Suresh Alse <alseambusher@gmail.com>
2017-03-24 16:04:43 -0700

    localization train and test

---
+++ config.py
@@ -0,0 +1,16 @@
+data_path = "/Users/alse/code/Medivh/data/"
+
+weight_path = data_path + 'caffe_layers_value.pickle'
+model_root = '/Users/alse/code/Medivh/models/'
+trainset_path = data_path + 'train.pickle'
+testset_path = data_path + 'test.pickle'
+label_dict_path = data_path + 'label_dict.pickle'
+pretrained_model_path = None #'../models/caltech256/model-0'
+n_epochs = 10000
+init_learning_rate = 0.01
+weight_decay_rate = 0.0005
+momentum = 0.9
+batch_size = 60
+
+dataset_path = data_path + 'caltech256'
+
